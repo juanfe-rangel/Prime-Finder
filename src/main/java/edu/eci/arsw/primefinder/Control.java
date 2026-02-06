9bd3d751272c7d4d9580c410dad9ce8e29c20986
juanfe-rangel <rangeljuanfe687@gmail.com>
2026-02-05 21:09:55 -0500

    final prime finder

--- src/main/java/edu/eci/arsw/primefinder/Control.java
+++ src/main/java/edu/eci/arsw/primefinder/Control.java
@@ -1,44 +1,79 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.eci.arsw.primefinder;
 
-/**
- *
- */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Control extends Thread {
-    
-    private final static int NTHREADS = 3;
-    private final static int MAXVALUE = 30000000;
-    private final static int TMILISECONDS = 5000;
+
+    private static final int NTHREADS = 3;
+    private static final int MAXVALUE = 30000000;
+    private static final int TMILISECONDS = 5000;
 
     private final int NDATA = MAXVALUE / NTHREADS;
+    private PrimeFinderThread[] pft;
+    private boolean paused = false;
 
-    private PrimeFinderThread pft[];
-    
     private Control() {
-        super();
-        this.pft = new  PrimeFinderThread[NTHREADS];
-
+        pft = new PrimeFinderThread[NTHREADS];
         int i;
-        for(i = 0;i < NTHREADS - 1; i++) {
-            PrimeFinderThread elem = new PrimeFinderThread(i*NDATA, (i+1)*NDATA);
-            pft[i] = elem;
+        for (i = 0; i < NTHREADS - 1; i++) {
+            pft[i] = new PrimeFinderThread(i * NDATA, (i + 1) * NDATA, this);
         }
-        pft[i] = new PrimeFinderThread(i*NDATA, MAXVALUE + 1);
+        pft[i] = new PrimeFinderThread(i * NDATA, MAXVALUE + 1, this);
     }
-    
+
     public static Control newControl() {
         return new Control();
     }
 
     @Override
     public void run() {
-        for(int i = 0;i < NTHREADS;i++ ) {
-            pft[i].start();
+        for (PrimeFinderThread t : pft) {
+            t.start();
+        }
+
+        while (true) {
+            try {
+                Thread.sleep(TMILISECONDS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            pauseAll();
+            showPrimes();
+            waitForEnter();
+            resumeAll();
         }
     }
-    
+
+    public synchronized void pauseAll() {
+        paused = true;
+    }
+
+    public synchronized void resumeAll() {
+        paused = false;
+        notifyAll();
+    }
+
+    public synchronized boolean isPaused() {
+        return paused;
+    }
+
+    private int showPrimes() {
+        int total = 0;
+        for (PrimeFinderThread t : pft) {
+            total += t.getPrimeCount();
+        }
+        System.out.println("Primos encontrados hasta ahora: " + total);
+        return total;
+    }
+
+    private void waitForEnter() {
+        System.out.println("Presione ENTER para continuar");
+        try {
+            new BufferedReader(new InputStreamReader(System.in)).readLine();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -3,45 +3,50 @@ package edu.eci.arsw.primefinder;
 import java.util.LinkedList;
 import java.util.List;
 
-public class PrimeFinderThread extends Thread{
+public class PrimeFinderThread extends Thread {
 
-	
-	int a,b;
-	
-	private List<Integer> primes;
-	
-	public PrimeFinderThread(int a, int b) {
-		super();
-                this.primes = new LinkedList<>();
-		this.a = a;
-		this.b = b;
-	}
+    private int a, b;
+    private List<Integer> primes;
+    private Control control;
 
-        @Override
-	public void run(){
-            for (int i= a;i < b;i++){						
-                if (isPrime(i)){
-                    primes.add(i);
-                    System.out.println(i);
+    public PrimeFinderThread(int a, int b, Control control) {
+        this.primes = new LinkedList<>();
+        this.a = a;
+        this.b = b;
+        this.control = control;
+    }
+
+    @Override
+    public void run() {
+        for (int i = a; i < b; i++) {
+
+            synchronized (control) {
+                while (control.isPaused()) {
+                    try {
+                        control.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
-	}
-	
-	boolean isPrime(int n) {
-	    boolean ans;
-            if (n > 2) { 
-                ans = n%2 != 0;
-                for(int i = 3;ans && i*i <= n; i+=2 ) {
-                    ans = n % i != 0;
-                }
-            } else {
-                ans = n == 2;
-            }
-	    return ans;
-	}
 
-	public List<Integer> getPrimes() {
-		return primes;
-	}
-	
+            if (isPrime(i)) {
+                primes.add(i);
+            }
+        }
+    }
+
+    boolean isPrime(int n) {
+        if (n < 2) return false;
+        if (n == 2) return true;
+        if (n % 2 == 0) return false;
+        for (int i = 3; i * i <= n; i += 2) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+
+    public int getPrimeCount() {
+        return primes.size();
+    }
 }
